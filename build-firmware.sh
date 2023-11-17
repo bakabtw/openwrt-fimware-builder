06cd53d4a239fe057d19a92b892422eabf583012
Eugene Konstantinov <me@knst.me>
2023-11-17 12:58:25 +0300

    Using variables in PATH

--- build-firmware.sh
+++ build-firmware.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+openwrt_version="23.05.0"
+imagebuilder_dir="openwrt-imagebuilder-$openwrt_version-ramips-mt76x8.Linux-x86_64"
+
 PROFILE="xiaomi_mi-router-4c"
 PACKAGES=""
 FILES="files"
@@ -9,33 +12,35 @@ OTA_UPDATER_REPO="https://github.com/bakabtw/openwrt-ota-updater.git"
 OTA_UPDATER_REPO_DIR="openwrt-ota-updater"
 
 # Cloning or updating git repo
-if [ ! -d "./$OTA_UPDATER_REPO_DIR/" ]; then
+if [ ! -d "$OTA_UPDATER_REPO_DIR" ]; then
     echo "Clonning repo ($OTA_UPDATER_REPO)"
     git clone "$OTA_UPDATER_REPO" "$OTA_UPDATER_REPO_DIR" || (echo "ERROR - Couldn't copy a repo ($OTA_UPDATER_REPO)" && exit 1)
 else
     echo "Updating repo ($OTA_UPDATER_REPO)"
     cd $OTA_UPDATER_REPO_DIR || return
     git pull || (echo "ERROR - Couldn't update a repo ($OTA_UPDATER_REPO)" && exit 1)
+    cd ..
 fi
 
 # Cleaning up old files
-if [ -d "./files/" ]; then
-    rm -rf ./files/*
+if [ -d "$imagebuilder_dir/files/" ]; then
+    echo "Cleaning up old files..."
+    rm -rf $imagebuilder_dir/files/*
 fi
 
 # Creating file structure
-mkdir -p files/etc/crontabs/
-mkdir -p files/usr/bin/
+mkdir -p "$imagebuilder_dir/files/etc/crontabs/"
+mkdir -p "$imagebuilder_dir/files/usr/bin/"
 
 # Copying files
-cp "./$OTA_UPDATER_REPO_DIR/ota-updater.sh" "./files/usr/bin/ota-updater.sh"
-cp "./$OTA_UPDATER_REPO_DIR/crontabs/ota-updater" "./files/etc/crontabs/ota-updater"
+cp "$OTA_UPDATER_REPO_DIR/ota-updater.sh" "$imagebuilder_dir/files/usr/bin/ota-updater.sh"
+cp "$OTA_UPDATER_REPO_DIR/crontabs/ota-updater" "$imagebuilder_dir/files/etc/crontabs/ota-updater"
 
 # Changing permissions
-chmod 755 "./files/usr/bin/ota-updater.sh"
-chmod 644 "./files/etc/crontabs/ota-updater"
-chown root:root "./files/usr/bin/ota-updater.sh"
-chown root:root "./files/etc/crontabs/ota-updater"
+chmod 755 "$imagebuilder_dir/files/usr/bin/ota-updater.sh"
+chmod 644 "$imagebuilder_dir/files/etc/crontabs/ota-updater"
+chown root:root "$imagebuilder_dir/files/usr/bin/ota-updater.sh"
+chown root:root "$imagebuilder_dir/files/etc/crontabs/ota-updater"
 
 # Building firmware
-make image PROFILE="$PROFILE" PACKAGES="$PACKAGES" FILES="$FILES" DISABLED_SERVICES="$DISABLED_SERVICES"
+cd $imagebuilder_dir && make image PROFILE="$PROFILE" PACKAGES="$PACKAGES" FILES="$FILES" DISABLED_SERVICES="$DISABLED_SERVICES"
