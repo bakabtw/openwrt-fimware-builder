da3f3511935f695772aaf7aa27a574f783da734a
Eugene Konstantinov <me@knst.me>
2023-11-17 13:17:11 +0300

    Code cleaning up

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-##############
-##
-## Simple script to download and unpack Image Builder for mt76x8 arch
-##
-##############
-
 openwrt_version="23.05.0"
 imagebuilder_dir="openwrt-imagebuilder-$openwrt_version-ramips-mt76x8.Linux-x86_64"
 imagebuilder_file="$imagebuilder_dir.tar.xz"
@@ -16,7 +10,7 @@ if [ ! -d "./$imagebuilder_dir/" ]; then
     wget -c $imagebuilder_url || (echo "ERROR - Couldn't download Image Builder ($imagebuilder_url)" && exit 1)
     wget -c $imagebuilder_sha256sum_url || (echo "ERROR - Couldn't download sha256sums ($imagebuilder_url)" && exit 1)
     echo "Download complete"
-    
+
     echo "Unpacking files..."
     tar -xJf $imagebuilder_file || (echo "ERROR - Couldn't unpack the archive ($imagebuilder_file)" && exit 1)
     echo "Image Builder was sucessfully installed."
