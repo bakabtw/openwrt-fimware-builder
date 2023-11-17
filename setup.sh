7aa0358f9a558400c8d6fd027cdbe75c52cc7afe
Eugene Konstantinov <me@knst.me>
2023-11-17 12:30:52 +0300

    Added setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-imagebuilder_url="https://downloads.openwrt.org/releases/23.05.0/targets/ramips/mt76x8/openwrt-imagebuilder-23.05.0-ramips-mt76x8.Linux-x86_64.tar.xz"
-imagebuilder_sha256sum_url="https://downloads.openwrt.org/releases/23.05.0/targets/ramips/mt76x8/sha256sums"
+##############
+##
+## Simple script to download and unpack Image Builder for mt76x8 arch
+##
+##############
 
-wget -c $imagebuilder_url
-wget -c $imagebuilder_sha256sum_url
+openwrt_version="23.05.0"
+imagebuilder_dir="openwrt-imagebuilder-$openwrt_version-ramips-mt76x8.Linux-x86_64"
+imagebuilder_file="$imagebuilder_dir.tar.xz"
+imagebuilder_url="https://downloads.openwrt.org/releases/$openwrt_version/targets/ramips/mt76x8/$imagebuilder_file"
+imagebuilder_sha256sum_url="https://downloads.openwrt.org/releases/$openwrt_version/targets/ramips/mt76x8/sha256sums"
 
-tar xJf openwrt-imagebuilder-*.tar.xz
-
+if [ ! -d "./$imagebuilder_dir/" ]; then
+    wget -c $imagebuilder_url || (echo "ERROR - Couldn't download Image Builder ($imagebuilder_url)" && exit 1)
+    wget -c $imagebuilder_sha256sum_url || (echo "ERROR - Couldn't download sha256sums ($imagebuilder_url)" && exit 1)
+    echo "Download complete"
+    
+    echo "Unpacking files..."
+    tar -xJf $imagebuilder_file || (echo "ERROR - Couldn't unpack the archive ($imagebuilder_file)" && exit 1)
+    echo "Image Builder was sucessfully installed."
+else
+    echo "Image Builder dir already exists. Please remove it before launching 'setup.sh'."
+    exit 1
+fi
